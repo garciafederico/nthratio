c90a7992b77164c923ec55806c0637bb2e497821
Federico Garcia <fgarcia1012002@gmail.com>
2021-11-14 11:03:08 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# NTHRATIO and RNTHCOMP, by Federico García
+
+RNTHCOMP is NTHCOMP XSPEC model, where the normalization is changed
+using a parameter that determines at which energy the model is unity
+for a unity norm.
+
+NTHRATIO is a multiplicative XSPEC model based on RNTHCOMP, used to
+first-order correct the soft-excess introduced by xillver-based models.
+
+This project is under development.
+Please contact the author before using it.
---
+++ nthratio.f
@@ -0,0 +1,31 @@
+      SUBROUTINE nthratio(Ear,Ne,Param,Ifl,Photar,Photer)
+      implicit none
+      INTEGER Ne , Ifl
+      REAL Param(*) , Ear(0:Ne) , Photar(Ne), Photer(Ne)
+      REAL PhotarkTin(Ne), Photar005(Ne)
+      REAL ParamRNTHCOMP(6)
+
+c     number of model parameters: 16
+c     1: photon spectral index
+c     2: plasma temperature in keV
+c     3: (disc)blackbody temperature in keV
+c     4: type of seed spectrum (0-blackbody, 1-diskbb)
+c     5: redshift
+c     6: Energy in keV at which the model is unity
+
+      ParamRNTHCOMP(1) = Param(1)
+      ParamRNTHCOMP(2) = Param(2)
+      ParamRNTHCOMP(3) = Param(3)
+      ParamRNTHCOMP(4) = 1
+      ParamRNTHCOMP(5) = 0
+      ParamRNTHCOMP(6) = 10
+
+      call rnthcomp(Ear,Ne,ParamRNTHCOMP,Ifl,PhotarkTin,Photer)
+      ParamRNTHCOMP(3) = 0.05
+      call rnthcomp(Ear,Ne,ParamRNTHCOMP,Ifl,Photar005,Photer)
+
+      photar = PhotarkTin/Photar005
+      photer = 0.
+
+      RETURN
+      END
